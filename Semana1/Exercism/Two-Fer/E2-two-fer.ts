c2f281c736be50eac745a30fdc651048475e0a07
Alexnarea <lanarea@sudamericano.edu.ec>
2024-04-11 11:54:16 -0500

    update

---
+++ Semana1/Exercism/Two-Fer/E2-two-fer.ts
@@ -0,0 +1,10 @@
+export function twoFer(name?: string): string {
+    const recipient = name || "you";
+    const message = `One for ${recipient}, one for me.`;
+    console.log(message); // Agregamos console.log para imprimir el mensaje
+    return message;
+  }
+  
+  // Ejemplo
+  twoFer("alex");
+  
